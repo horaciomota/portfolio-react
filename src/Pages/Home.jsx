2e3c753587a3fa560d51ab17d6bb94e852e6cd3f
Horacio Mota <horaciomota93@gmail.com>
2024-03-19 18:02:57 +0000

    refatort: Update home component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,11 @@
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
+import "../styles/global/_boilerplate.scss"
 
 export default function Home() {
     return(
         <>
         <Hero/>
-        <h1>teste</h1>
         <Projects/>
         </>
     )
