b10bb4e7c675f9012b38809299cee8a18384e774
Horacio Mota <horaciomota93@gmail.com>
2024-03-19 21:10:07 +0000

    refactor: Comment out get in touch component

---
+++ src/Pages/Experience.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import InfoCard from '../components/InfoCard';
+import GetInTouch from '../components/GetInTouch';
+import "../styles/about_styles/about.modules.scss";
+
+function About() {
+  return (
+    <section className="About-section">
+      <div className="container">
+        <div className='list-comp'>
+        <InfoCard
+          title="Experience"
+          subtitle="I’m a Swedish, Lead Designer & Design Director currently based in Brisbane, Australia. I love what I do and have done so for the last 20 years working on various projects and brands around the world. As a director, I draw upon my experience in design, concept development and team management to bring a comprehensive and creative process to all projects."
+          isAboutMe={true}
+
+        />
+        <InfoCard
+          title="Quick summary of my career:"
+          titleClassName="custom-title"
+          items={[
+            "20 years design experience",
+            "6 years at Disney & Star Wars",
+            "12 years at digital agencies",
+            "9 years of directing"
+          ]}
+        />
+        </div>
+        <div className='border-divider'></div>
+
+      </div> 
+    </section>
+  );
+}
+
+export default About;
